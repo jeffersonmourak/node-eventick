a4f06e5a2f8f4f711aba95133e42c16083108fff
jeffersonmourak <jefinho.moura14@gmail.com>
2015-10-14 22:02:17 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+(function(){
+	"use strict";
+
+	var token = require("./core/authentication");
+	var Events = require("./core/events");
+
+	function Eventick(email, password){
+		this.token = token(email,password);
+		this.events = new Events(this.token);
+	}
+
+	
+
+})();
